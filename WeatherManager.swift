e016f2f9bf427e7ad5dbf0845f801995ca0769a3
Michel Kim <kim98.michel@gmail.com>
2023-07-07 14:05:44 -0300

    Create WeatherModel and Computed Property

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -41,7 +41,13 @@ struct WeatherManager {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             print(decodedData.name)
             print(decodedData.main.temp)
-            print(decodedData.weather[0].description) 
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            weather.conditionName
         } catch {
             print(error)
         }
