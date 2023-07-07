56a6eb4ad4ea32428d31eb396c372c16cc5e6d5d
Michel Kim <kim98.michel@gmail.com>
2023-07-06 23:24:34 -0300

    JSON decoding

---
+++ WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Michel Jaejin Kim on 06/07/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
